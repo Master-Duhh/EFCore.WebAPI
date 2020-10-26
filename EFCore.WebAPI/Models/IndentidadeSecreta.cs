7e932edbf82ea78c70291af5a09f1034be16115f
Eduardo Romao <64819498+Master-Duhh@users.noreply.github.com>
2020-10-25 22:52:20 -0300

    Adicionar arquivos de projeto.

---
+++ EFCore.WebAPI/Models/IndentidadeSecreta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebAPI.Models
+{
+    public class IndentidadeSecreta
+    {
+        public int Id { get; set; }
+
+        public int NomeReal { get; set; }
+
+        public int HeroiId { get; set; }
+
+        public Heroi Heroi { get; set; }
+
+
+    }
+}
